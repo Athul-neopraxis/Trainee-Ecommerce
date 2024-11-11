3102b87da3448934633ae98afe2c67ef8a8cab65
hashilnisam-neopraxis <hashil.nisam@neopraxis.in>
2024-11-11 11:40:43 +0530

    feature/pdp added

---
+++ assets/js/pdp.js
@@ -0,0 +1,14 @@
+{
+    "tech": [
+        {
+            "name" : "hashil",
+            "age" : "10",
+            "email" : "hashilnizam@gmail.com"
+        },
+        {
+            "name" : "athul",
+            "age" : "10",
+            "email" : "athul@gmail.com"
+        }
+    ]
+}
